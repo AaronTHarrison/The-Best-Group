d574eda94b0884063f2f064b9890fa9641afb537
Aaron Harrison <39066498+AaronTHarrison@users.noreply.github.com>
2018-06-08 20:39:13 -0700

    Delete main.js

--- FinalGameFirstPrototype/WorldWarTrail/js/main.js
+++
@@ -1,7 +0,0 @@
-var game = new Phaser.Game(1000, 600, Phaser.AUTO);
-game.state.add('Boot', Boot);
-game.state.add('Preloader', Preloader);
-game.state.add('Instructions', Instructions);
-game.state.add('Scenarios', Scenarios);
-game.state.add('Gameover', Gameover);
-game.state.start('Boot');
