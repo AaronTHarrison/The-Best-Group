bf470abd6b0ff9b748782f24d7151479d1db2351
Aaron Harrison <39066498+AaronTHarrison@users.noreply.github.com>
2018-06-08 20:36:41 -0700

    updating and fixing github

---
+++ WorldWarTrail/js/instructions.js
@@ -0,0 +1,30 @@
+function Instructions() {
+		var text1;
+		var text2;
+};
+
+// tell player how to play, then move on to first scenario
+Instructions.prototype = {
+		init: function() {
+				console.log('Instructions: init');
+		},
+		preload: function() {
+				console.log('Instructions: preload');
+				text1 = this.add.text(0, 0, 'You are a WW2 paratrooper who has crashed behind enemy lines. And man, does it suck. You must choose your actions wisely to survive\n', {fontSize: '30px', fill: 'black', wordWrap : true, wordWrapWidth: 1000});
+				text2 = this.add.text(0, 0, 'Press the arrow corresponding to your choice.\nGood luck...', {fontSize: '30px', fill: 'black'});
+				text1.setTextBounds(0,600,1000,100);
+				text2.setTextBounds(0,700,1000,100);
+		},
+		create: function() {
+				this.stage.backgroundColor = '#859994';
+		},
+		update: function() {
+				// make the text move up the screen, fade out when spacebar pressed
+				text1.position.y -= 1;
+				text2.position.y -= 1;
+				if(this.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)){
+						game.camera.fade(0x000000, 1000);
+						game.time.events.add(Phaser.Timer.SECOND * 3, function() { game.state.start('Preloader')});
+				}
+		}
+};
